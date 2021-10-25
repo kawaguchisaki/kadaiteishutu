bb6906f82ff5bf7053218849b3afcc220d7846d8
kawaguchi saki <amsn17.saki15@gmail.com>
2021-10-25 02:30:10 +0000

    PHP/Laravel04saiteishutu

--- 3practice.php
+++ 3practice.php
@@ -46,36 +46,41 @@ function max_array($arr){
     }
     return $max_number;
 }
+
 echo max_array([3,6,2,9,1]);
 echo "\n";
 
 //課題５
+//strip_tags
 
 $str = "<h1>ここのタグは消す</h1><p>ここのタグは残す</p><h2>残す</h2><p>残す</p>";
 echo strip_tags($str,"<p><h2>");
 echo "\n";
 
-
+//array_push
 $stack =["a","b"];
 array_push($stack,"c","d");
 var_dump($stack);
 echo "\n";
 
+//array_merge
 $array1 = ["a","b"];
 $array2 = ["c","d","e"];
 $array3 = ["f","g"];
 $allarray = array_merge($array1,$array2,$array3);
 print_r($allarray);
 
-
+//time
 echo time();
 echo "\n";
 
-
+//mktime
 $time = mktime(0, 0, 0, 1, 1, 2021);
 print_r(date("Y年m月d日h時i分s秒",$time));
 echo "\n";
 
+
+//date
 echo date('Y-m-d H:i:s');
 echo "\n";
 ?>
