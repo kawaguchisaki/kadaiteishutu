e77f60de3c63990fb4cafa277ac48a243ca8fbcd
kawaguchi saki <amsn17.saki15@gmail.com>
2021-10-25 02:03:30 +0000

    PHP/Laravel04saiteishutu

--- 3practice.php
+++ 3practice.php
@@ -1,9 +1,8 @@
 <?php
-/*
-課題１
+
+//課題１
 function double($max){
-    $result = $max*2;
-    return $result;
+   return $max*2;
 }
 echo double(2);
 echo "\n";
@@ -12,10 +11,9 @@ echo double(9);
 echo "\n";
 
 
-課題２
+//課題２
 function add($a, $b){
-    $result = $a + $b;
-    return $result;
+   return $a + $b;
 }
 echo add(1,2);
 echo "\n";
@@ -25,19 +23,19 @@ echo "\n";
 
 
 
-課題３
+//課題３
 function multiply($arr){
     $result = 1;
     foreach($arr as $a){
         $result *= $a;
-}
+    }
     return $result;
 }
 echo multiply(array(1,3,5,7,9));
 echo "\n";
 
 
-課題４
+//課題４
 
 function max_array($arr){
     $max_number = $arr[0];
@@ -49,16 +47,19 @@ function max_array($arr){
     return $max_number;
 }
 echo max_array([3,6,2,9,1]);
+echo "\n";
 
-課題５
+//課題５
 
 $str = "<h1>ここのタグは消す</h1><p>ここのタグは残す</p><h2>残す</h2><p>残す</p>";
 echo strip_tags($str,"<p><h2>");
+echo "\n";
 
 
 $stack =["a","b"];
 array_push($stack,"c","d");
 var_dump($stack);
+echo "\n";
 
 $array1 = ["a","b"];
 $array2 = ["c","d","e"];
@@ -68,11 +69,14 @@ print_r($allarray);
 
 
 echo time();
-*/
+echo "\n";
+
 
 $time = mktime(0, 0, 0, 1, 1, 2021);
 print_r(date("Y年m月d日h時i分s秒",$time));
+echo "\n";
 
 echo date('Y-m-d H:i:s');
+echo "\n";
 ?>
 
--- example.php
+++ example.php
@@ -1,3 +1,25 @@
 <?php
-echo 'hello php!';
+function sum1($a, $b){
+    $a + $b;
+}
+echo sum1(3,4) + 10 ."\n";
+
+function sum2($a, $b){
+    return $a + $b;
+}
+echo sum2(3,4) + 10 ."\n";
+
+function sum3($a, $b){
+    $result = $a + $b;
+    return 100;
+}
+echo sum3(3,4) + 10 ."\n";
+
+function sum4($a, $b){
+  $result = $a + $b;
+  echo "hoge1\n";
+  return 100;
+  $a + b;
+}
+echo sum4(3,4) + 10 ."\n";
 ?>
